bf13f26de7ea23cb5f1eb4f83a376bfb349e7689
abdu-1 <abduulrahmanj@gmail.com>
2026-02-17 11:30:44 +0200

    Init commint setup Express server, folder structure, and DB connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/database");
+
+// Connect with db
+dbConnection();
+
+//Express app
+const app = express();
+
+//Middleware
+app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Mount Route
+
+const PORT = process.env.PORT || 9000;
+app.listen(process.env.PORT, () => {
+  console.log(`App running on ${PORT}`);
+});
