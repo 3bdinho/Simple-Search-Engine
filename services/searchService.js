2e27a6490670399f0a0f143e170906b4e0d521d0
abdu-1 <abduulrahmanj@gmail.com>
2026-02-19 14:12:00 +0200

    Add boolean search feature with service and route

--- services/searchService.js
+++ services/searchService.js
@@ -23,3 +23,24 @@ exports.searchHandler = asyncHandler(async (req, res, next) => {
     })),
   });
 });
+
+exports.SearchBooleanHandler = asyncHandler(async (req, res, next) => {
+  const { q } = req.query;
+  if (!q) {
+    return next(new ApiError('Query parameter "q" is required', 400));
+  }
+
+  const results = SearchEngine.booleanSearch(q);
+
+  res.status(200).json({
+    status: "success",
+    query: q,
+    resultCount: results.length,
+    results: (await results).map((doc) => ({
+      id: doc._id,
+      title: doc.title,
+      url: doc.url,
+      excerpt: doc.content.substring(0, 200) + "...",
+    })),
+  });
+});
